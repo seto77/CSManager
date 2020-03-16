8a25e89e31a27a59aaea1396de63771d5c61fd65
seto <seto77@gmail.com>
2020-03-17 00:42:42 +0900

    20200317

--- CSManager/Properties/AssemblyInfo.cs
+++ CSManager/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2020.3.14.1055")]
-[assembly: AssemblyFileVersion("2020.3.14.1055")]
+[assembly: AssemblyVersion("2020.3.16.1534")]
+[assembly: AssemblyFileVersion("2020.3.16.1534")]
