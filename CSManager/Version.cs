a63baa4edad23f978de557aa6a84dbd5a4ba460f
seto <seto77@live.jp>
2024-11-13 18:00:45 +0900

    update

--- CSManager/Version.cs
+++ CSManager/Version.cs
@@ -11,11 +11,12 @@ namespace CSManager
            ;
         static public string VersionAndDate { get => History.Remove(0, 10).Remove(20); }
 
-        static public int AMCSD = 21_026;
-        static public int COD = 514_854;
+        static public int AMCSD = 21_044;
+        static public int COD = 519_205;
 
         static public string History = 
             "History" +
+                "\r\n ver1.898(2024/11/13) Updated .Net Desktop Runtime to 9.0. Renewed AMCSD database (contains 21,044) and COD database (519,205)." +
                 "\r\n ver1.897(2024/06/30) Renewed AMCSD database (contains 21,026) and COD database (514,854)." +
                 "\r\n ver1.896(2024/01/04) Updated .Net Desktop Runtime to 8.0. Fixed a broken link to manual pages." +
                 "\r\n ver1.895(2023/09/29) Renewed AMCSD database (contains 21,004) and COD database (506,047)." +
